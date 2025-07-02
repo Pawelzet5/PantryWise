712f6b8bb8d20ce87574024e6857d99fda11f154
PawelZielinski <pawel.zielinski@bitapps.fi>
2025-07-02 23:03:42 +0200

    ADD: category, expiration date to Product

--- app/src/main/java/com/example/pantrywise/model/dataclass/Product.kt
+++ app/src/main/java/com/example/pantrywise/model/dataclass/Product.kt
@@ -5,8 +5,10 @@ import com.example.pantrywise.model.enums.ProductUnit
 
 
 data class Product(
+    val id: Int = 0,
     val name: String,
     var quantity: Double,
     var productUnit: ProductUnit,
-    var category: ProductCategory = ProductCategory.OTHER
+    var category: ProductCategory = ProductCategory.OTHER,
+    var expirationDate: Long? = null
 )
